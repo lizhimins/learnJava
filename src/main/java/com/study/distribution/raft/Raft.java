df81e5fea922faae1ddeeb849591119bf0445f3c
lizhimins <707364882@qq.com>
2020-03-10 23:58:43 +0800

    Study Concurrent

---
+++ src/main/java/com/study/distribution/raft/Raft.java
@@ -0,0 +1,22 @@
+package com.study.distribution.raft;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Raft {
+    private Timer timer = new Timer();
+
+    public static void main(String[] args) {
+        int gapTime = new Random().nextInt() % 150 + 150;
+
+        System.out.println(gapTime);
+
+        new Raft().timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(System.currentTimeMillis());
+            }
+        }, 0, gapTime);
+    }
+}
